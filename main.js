be8f70eb2eb1a57c24ed9cd802ac9871960554aa
fidel50 <120042348+fidel50@users.noreply.github.com>
2026-04-29 22:08:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -409,14 +409,14 @@ function confirmOrder() {
     //const notes = document.getElementById('customerNotes').value || 'Sin notas adicionales';
     const deliveryOptions = document.querySelectorAll('input[name="deliveryPoint"]');
     //const selectedDeliveryPoint = document.getElementById('selectedDeliveryPoint');
-    alert(deliveryOptions);
-    /*const recojo;
+    //alert(deliveryOptions);
+    const recojo = document.getElementById('point1').value;
     deliveryOptions.forEach(option => {
         option.addEventListener('change', function () {
             //selectedDeliveryPoint.textContent = "Elegiste: " + this.value;
             recojo = this.value; // aquí puedes usar el valor
         });
-    });*/
+    });
     
     if (!name || !phone) {
         alert('Por favor completa todos los campos obligatorios (*)');
